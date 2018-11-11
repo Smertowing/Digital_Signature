eab583da4e177ace2129da91cb8cc5d79941dfc7
Kiryl <Kiryl.co@gmail.com>
2018-11-11 22:53:45 +0300

    Initial Commit

---
+++ Digital_Signature/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Digital_Signature
+//
+//  Created by Kiryl Holubeu on 11/11/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
